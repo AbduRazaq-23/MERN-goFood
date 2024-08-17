2e8fd15ba3395eba023e0ae3df5b38e1f9d8f9de
Abdurazaq333 <irazaqr986@gmail.com>
2024-08-17 16:38:32 +0500

    add option field

--- frontend/goFood/src/screens/LogIn.jsx
+++ frontend/goFood/src/screens/LogIn.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
 
 const UserLogInForm = () => {
   const navigate = useNavigate();
@@ -9,6 +8,7 @@ const UserLogInForm = () => {
   const [formData, setFormData] = useState({
     email: "",
     password: "",
+    type: "",
   });
 
   const handleChange = (e) => {
@@ -21,21 +21,25 @@ const UserLogInForm = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await axios.post(
-      "http://localhost:8000/api/v1/users/login",
-      formData,
-      { withCredentials: true }
-    );
+    try {
+      const response = await axios.post(
+        "http://localhost:8000/api/v1/users/login",
+        formData,
+        { withCredentials: true }
+      );
 
-    setFormData("");
-    navigate("/");
+      setFormData({ email: "", password: "", type: "" });
+      navigate("/");
+    } catch (error) {
+      console.error("Error logging in:", error);
+    }
   };
 
   return (
     <div className="h-screen flex items-center">
-      <div className="w-full md:w-[30%] mx-auto   p-8 my-auto bg-white shadow-md rounded-md">
+      <div className="w-full md:w-[30%] mx-auto p-8 my-auto bg-white shadow-md rounded-md">
         <h2 className="text-2xl text-gray-700 text-center font-bold mb-4">
-          SignIn
+          Sign In
         </h2>
         <form onSubmit={handleSubmit}>
           <div className="mb-4">
@@ -76,17 +80,38 @@ const UserLogInForm = () => {
             />
           </div>
 
+          <div className="mb-4">
+            <label
+              htmlFor="type"
+              className="block text-sm font-medium text-gray-700 mb-2"
+            >
+              Type
+            </label>
+            <select
+              id="type"
+              name="type"
+              value={formData.type}
+              onChange={handleChange}
+              className="w-full px-3 py-2 border text-gray-700 border-gray-300 rounded-md bg-white shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
+              required
+            >
+              <option value="">-- Choose Type --</option>
+              <option value="admin">admin</option>
+              <option value="user">user</option>
+            </select>
+          </div>
+
           <button
             type="submit"
             className="w-full px-4 py-2 bg-indigo-600 text-white font-semibold rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500"
           >
-            SignIn
+            Sign In
           </button>
+
           <p className="text-gray-700 mt-4 text-center">
-            if u don't have an account{" "}
+            Don't have an account?{" "}
             <Link to="/register">
-              {" "}
-              <span className="text-blue-600">SignUp</span>
+              <span className="text-blue-600">Sign Up</span>
             </Link>
           </p>
         </form>
