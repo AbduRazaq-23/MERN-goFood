ac67edbd5442d42510b901b5bf1cdd4e2d51907f
Abdurazaq333 <irazaqr986@gmail.com>
2024-08-18 22:21:28 +0500

    working on add food form

--- frontend/goFood/src/App.jsx
+++ frontend/goFood/src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./screens/Home";
 import UserRegisterForm from "./screens/Register";
 import UserLogInForm from "./screens/LogIn";
 import Dashboard from "./screens/Dashboard";
+import AddFoodForm from "./screens/addFoodForm";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -16,6 +17,7 @@ function App() {
         <Route path="/login" element={<UserLogInForm />} />
 
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/addfood" element={<AddFoodForm />} />
       </Routes>
     </Router>
   );
---
+++ frontend/goFood/src/screens/AddFoodForm.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const AddFoodForm = () => {
+  return (
+    <div>
+      <form>
+        <h1>hi</h1>
+        <button className="text-gray-200" type="submit">
+          Add
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default AddFoodForm;
